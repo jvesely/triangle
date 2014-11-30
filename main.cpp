6bcac638a527e635d62d1040dbc5f3f932d9e5ae
Jan Vesely <jan.vesely@rutgers.edu>
2014-11-30 15:17:00 -0500

    Initailze d vector.
    Add is_zero helper for vectors
    
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>

--- main.cpp
+++ main.cpp
@@ -8,12 +8,22 @@
 
 using num_type = double;
 using vector = typename ::boost::numeric::ublas::vector<num_type>;
+using scalar_vector = typename ::boost::numeric::ublas::scalar_vector<num_type>;
 using matrix = typename ::boost::numeric::ublas::matrix<num_type>;
+
+template<typename T>
+bool is_zero(const T &);
+
+template<>
+bool is_zero<vector>(const vector &v)
+{
+	return !::std::any_of(v.begin(), v.end(),
+		              [](const num_type n){ return n != 0; });
+}
 
 int main(void)
 {
 	unsigned dims = 0;
-//	::std::cout << "Set number of dimensions: ";
 	::std::cin >> dims;
 
 	::std::vector<vector> points;
@@ -24,8 +34,7 @@ int main(void)
 			::std::cin >> tmp;
 			v(i) = tmp;
 		}
-		if (::std::any_of(v.begin(), v.end(),
-		                  [](const num_type n){ return n != 0; }))
+		if (!is_zero(v))
 			points.push_back(v);
 		else
 			break;
@@ -39,6 +48,11 @@ int main(void)
 			A(j,i) = points[i][j];
 		}
 	}
+
+	const num_type n = points.size();
+	vector d(scalar_vector(n, 1.0 / n));
+
 	::std::cout << A << ::std::endl;
+	::std::cout << d << ::std::endl;
 	return 0;
 }
